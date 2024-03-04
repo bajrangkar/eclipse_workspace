c4db491cf17bbf04733b93470afb13dfd3b5f60e
B K <bajrang1029@gmail.com>
2024-03-04 17:52:33 +0530

    preserving the order

--- Programs_Twisters/src/interview/drills/strings/countings/WordCounting.java
+++ Programs_Twisters/src/interview/drills/strings/countings/WordCounting.java
@@ -1,6 +1,7 @@
 package intervire.drills.strings.countings;
 
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -11,8 +12,9 @@ public class WordCounting {
 		String str = "bull dog acts as bull not as dog";
 		
 		List<String> stringList = Arrays.asList(str.split(" "));
+		System.out.println(stringList);
 		Map<String, Long> mapResult = stringList.stream()
-										.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+										.collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()));
 		System.out.println(mapResult);
 	}
 }
